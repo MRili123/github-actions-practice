829d791c232544297544a94e3277f3cf8340e99e
MRili123 <iliasssjb2004@gmail.com>
2026-04-13 20:58:41 +0100

    workflow_p2

--- calcul.py
+++ calcul.py
@@ -1,8 +1,8 @@
-# def addition(a, b):
-#  return a + b
-# def multiplication(a, b):
-#  return a * b
-# def division(a, b):
-#  if b == 0:
-#   raise ValueError("Divisionpar zéro interdite")
-#  return a / b
+def addition(a, b):
+ return a + b
+def multiplication(a, b):
+ return a * b
+def division(a, b):
+ if b == 0:
+  raise ValueError("Divisionpar zéro interdite")
+ return a / b
